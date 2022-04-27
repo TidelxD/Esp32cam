a7f80c8f84ea2804d7b54c40536abdebd08e45c0
YaguerNermine <61780777+YaguerNermine@users.noreply.github.com>
2022-04-27 15:53:19 +0200

    esp32cam with firebase

---
+++ esp_fire.ino
@@ -0,0 +1,299 @@
+#include "WiFi.h"
+#include "esp_camera.h"
+#include "Arduino.h"
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "driver/rtc_io.h"
+#include <SPIFFS.h>
+#include <FS.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include <addons/TokenHelper.h>
+#include "addons/RTDBHelper.h"
+
+//Replace with your network credentials
+const char* ssid = "LTE4G-B310-7976B";
+//const char* ssid = "Galaxy A528302";
+const char* password = "68749903051012Yaguer";
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyA8ZZ7xAB7wBp-tzOiVkMo2G9StpT3-PH4"
+#define DATABASE_URL "esp32cam-110e7-default-rtdb.firebaseio.com"
+
+#define FIREBASE_AUTH "smte0Ret0zZ5c8JFuMmyuvemr7PzxLUac5FC5Hih"                    // the secret key generated from firebase
+
+//#define USER_EMAIL "feradjyounes08@gmail.com"
+//#define USER_PASSWORD "pmgKgTLcdDabNXXCfQcNeu5wl3f2"
+#define USER_EMAIL "nerminyaguer@gmail.com"
+#define USER_PASSWORD "19990708N"
+
+#define STORAGE_BUCKET_ID "esp32cam-110e7.appspot.com"
+
+#define FILE_PHOTO "/data/photo.jpg"
+
+// OV2640 camera module pins (CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+boolean takeNewPhoto = true;
+//Define Firebase Data objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig configF;
+FirebaseConfig config;
+
+bool taskCompleted = false;
+unsigned long sendDataPrevMillis = 0;
+int intValue;
+float floatValue;
+String downloadURL;
+int count = 0;
+//......................... LED 
+String fireStatus = "";                                                     // led status received from firebase
+
+int led = 2;                                                        
+
+
+bool signupOK = false;
+
+// Check if photo capture was successful
+bool checkPhoto( fs::FS &fs ) {
+  File f_pic = fs.open( FILE_PHOTO );
+  unsigned int pic_sz = f_pic.size();
+  return ( pic_sz > 100 );
+}
+
+// Capture Photo and Save it to SPIFFS
+void capturePhotoSaveSpiffs( void ) {
+  camera_fb_t * fb = NULL; // pointer
+  bool ok = 0; // Boolean indicating if the picture has been taken correctly
+  do {
+    // Take a photo with the camera
+    Serial.println("Taking a photo...");
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+     // Photo file name
+    Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+    File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+    // Insert the data in the photo file
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    }
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.print("The picture has been saved in ");
+      Serial.print(FILE_PHOTO);
+      Serial.print(" - Size: ");
+      Serial.print(file.size());
+      Serial.println(" bytes");
+    }
+        // Close the file
+    file.close();
+    esp_camera_fb_return(fb);
+
+    // check if file has been correctly saved in SPIFFS
+    ok = checkPhoto(SPIFFS);
+  } while ( !ok );
+}
+
+void initWiFi(){
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+}
+
+void initSPIFFS(){
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    ESP.restart();
+  }
+  else {
+    delay(500);
+    Serial.println("SPIFFS mounted successfully");
+  }
+}
+
+void initCamera(){
+ // OV2640 camera module
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  } 
+}
+
+void setup() {
+  // Serial port for debugging purposes
+Serial.begin(115200);
+   
+
+initWiFi();
+initSPIFFS();
+// Turn-off the 'brownout detector'
+WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+initCamera();
+// Assign the api key
+configF.api_key = API_KEY;
+config.database_url = DATABASE_URL;
+//Assign the user sign in credentials
+auth.user.email = USER_EMAIL;
+auth.user.password = USER_PASSWORD;
+//Assign the callback function for the long running token generation task
+configF.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+Firebase.begin(&configF, &auth);
+
+Firebase.reconnectWiFi(true);
+
+// connecter la carte a le réseau
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+}
+
+void loop() {
+ 
+  if (takeNewPhoto) {
+  capturePhotoSaveSpiffs();
+  takeNewPhoto = false;
+}
+if (Firebase.ready() && !taskCompleted){
+  taskCompleted = true;
+  Serial.print("Uploading picture... ");
+
+  //MIME type should be valid to avoid the download problem.
+  //The file systems for flash and SD/SDMMC can be changed in FirebaseFS.h.
+   if (Firebase.Storage.upload(&fbdo, STORAGE_BUCKET_ID /* Firebase Storage bucket id */, FILE_PHOTO /* path to local file */, mem_storage_type_flash /* memory storage type, mem_storage_type_flash and mem_storage_type_sd */, FILE_PHOTO /* path of remote file stored in the bucket */, "image/jpeg" /* mime type */)){
+    Serial.printf("\nDownload URL: %s\n", fbdo.downloadURL().c_str());
+  }
+  else{
+    Serial.println(fbdo.errorReason());
+  }
+}
+Serial.println(  Firebase.Storage.upload(&fbdo, STORAGE_BUCKET_ID, FILE_PHOTO, mem_storage_type_flash, FILE_PHOTO, "image/jpeg"));
+
+delay(5000);
+
+// pour real time 
+if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+
+    // ON ESSAI 
+    //MIME type should be valid to avoid the download problem.
+  //The file systems for flash and SD/SDMMC can be changed in FirebaseFS.h.
+
+ 
+  } 
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+          Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+   if (Firebase.RTDB.set(&fbdo, FILE_PHOTO, "/data/photo.jpg" , fbdo.downloadURL().c_str())){
+    Serial.println("fbdo.downloadURL().c_str() " + fbdo.dataPath());
+    Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+      
+  
+   }
+   else{
+    Serial.println(fbdo.errorReason());
+  }
+  }
+}
+
+
+  
